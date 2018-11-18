0f24e4b0a5cbe6fca1223c4cf9a23dad857d58b9
Brian Welch <myself@brianwelch.se>
2018-11-18 13:16:05 +0100

    lots fo folders and structure added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,12 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :design]
   def home
+
   end
+
+  def design
+    @title = "Designing Reclaim Dinner"
+  end
+
+
 end
