76ea56627fe46254f7a628df2d4d0356c25c9c86
Beltran Sainz de Vicuna Nowack <gorthor@gmail.com>
2018-11-19 15:18:56 +0100

    changed typos in models

--- app/models/alergy_user.rb
+++
@@ -1,4 +0,0 @@
-class AlergyUser < ApplicationRecord
-  belongs_to :alergy
-  belongs_to :user
-end
--- test/models/allergy_user_test.rb
+++ test/models/allergy_user_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class AlergyTest < ActiveSupport::TestCase
+class AllergyUserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
