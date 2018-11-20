07236f179ff7da0b0efd397eb61035697b6333ff
Brian Welch <myself@brianwelch.se>
2018-11-20 14:20:40 +0100

    migration changes - correcting tense

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  protect_from_forgery with: :exception
+  before_action :store_user_location!, if: :storable_location?
   before_action :authenticate_user!
 end
