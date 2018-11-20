91fb83675f1a468cf820b92e129a2033d7360f41
Brian Welch <myself@brianwelch.se>
2018-11-20 13:48:17 +0100

    recipe seed file works to seed - destroying does not work

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,6 @@
 class Recipe < ApplicationRecord
-  has_many :recipe_ingredients
-  has_many :user_recipes
-  has_many :recipe_ratings
-
-  belongs_to :categories
-  belongs_to :sub_categories
-  belongs_to :spicy_levels
+  has_many :recipe_ingredients, dependent: :destroy
+  # has_many :user_recipes
+  # has_many :recipe_ratings
+  # has_many :category_recipes
 end
--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,6 +1,3 @@
 class RecipeIngredient < ApplicationRecord
-  belongs_to :ingredients
-  belongs_to :imperial_measures
-  belongs_to :metric_measures
-  belongs_to :recipes
+  # belongs_to :recipes
 end
