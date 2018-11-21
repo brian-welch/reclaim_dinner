6a136411fc80e6e1f6d21743bd3afc39fe944167
Brian Welch <myself@brianwelch.se>
2018-11-21 05:29:09 +0100

    placement of recipe card into index

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,6 +2,12 @@ class RecipesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :show ]
   before_action :set_recipe, only: [:show]
 
+  def index
+    @seven_recipes = []
+    7.times { @seven_recipes << Recipe.all[rand(0..Recipe.count)] }
+    @seven_recipes
+  end
+
   def show
     @recipe_ingredients = Recipe.joins(:recipe_ingredients).where("recipe_ingredients.recipe_id = #{params[:id]}")
   end
