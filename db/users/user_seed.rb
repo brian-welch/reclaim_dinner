05aea110709659609ef4451ab177952df9db6e6a
Brian Welch <myself@brianwelch.se>
2018-11-21 08:11:36 +0100

    user data file created

---
+++ db/users/user_seed.rb
@@ -0,0 +1,66 @@
+users = [
+  {
+    first_name: "brian",
+    last_name: "welch",
+    email: "myself@brianwelch.se",
+    password: "12345678",
+    user_name: "B-Dubs",
+    birthday: "10-12-1974",
+    street_address: "Vimpelgatan 17 lgh. 103",
+    city: "malmö",
+    county: "skåne",
+    state: "",
+    post_code: "21114",
+    country: "Sweden",
+    facebook_username: "",
+    instagram_username: "",
+    pinterest_username: "",
+    children: false,
+    time_preference: 60,
+    t.bigint "food_preference_user_id"
+    t.bigint "allergy_user_id"
+    t.bigint "special_diet_user_id"
+  }, {
+    first_name: "beltrán",
+    last_name: "sainz de vicuña nowack",
+    email: "gorthor@gmail.com",
+    password: "12345678",
+    user_name: "",
+    birthday: "",
+    street_address: "",
+    city: "",
+    county: "",
+    state: "",
+    post_code: "",
+    country: "",
+    facebook_username: "",
+    instagram_username: "",
+    pinterest_username: "",
+    children: false,
+    time_preference: 60,
+    t.bigint "food_preference_user_id"
+    t.bigint "allergy_user_id"
+    t.bigint "special_diet_user_id"
+  }, {
+    first_name: "makena",
+    last_name: "hawley",
+    email: "makena.hawley@gmail.com",
+    password: "12345678",
+    user_name: "",
+    birthday: "",
+    street_address: "",
+    city: "",
+    county: "",
+    state: "",
+    post_code: "",
+    country: "",
+    facebook_username: "",
+    instagram_username: "",
+    pinterest_username: "",
+    children: false,
+    time_preference: 60,
+    t.bigint "food_preference_user_id"
+    t.bigint "allergy_user_id"
+    t.bigint "special_diet_user_id"
+  }
+]
