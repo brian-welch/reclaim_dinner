77eeb46d68e4e968c8793d5f2e222b8660102468
Beltran Sainz de Vicuna Nowack <gorthor@gmail.com>
2018-11-22 16:48:08 +0100

    animated card now slides up from footer

--- app/javascript/components/recipe-information-toggle.js
+++ app/javascript/components/recipe-information-toggle.js
@@ -2,10 +2,7 @@ function recipeInformationToggle() {
 const recipeImg = document.querySelectorAll(".dish-image");
 
   $(".dish-image").hover(function () {
-    if ($(".info-card").hasClass("slideup"))
-      $(".info-card").removeClass("slideup").addClass("slidedown");
-    else
-      $(".info-card").removeClass("slidedown").addClass("slideup");
+    $(".info-card").toggleClass("open");
   });
 };
 export { recipeInformationToggle };
