1ecff81ffe7996300abb7971a21822cf42ef93df
Beltran Sainz de Vicuna Nowack <gorthor@gmail.com>
2018-11-22 17:54:06 +0100

    fixed gap after last image

--- app/javascript/components/recipe-information-toggle.js
+++ app/javascript/components/recipe-information-toggle.js
@@ -1,10 +1,11 @@
 function recipeInformationToggle() {
 const recipeImg = document.querySelectorAll(".dish-image");
 
-  $(".dish-image").hover(function () {
+  $(".dish-image, .info-card").hover(function () {
     $(".info-card").toggleClass("open");
   });
 };
 export { recipeInformationToggle };
+
 
 
