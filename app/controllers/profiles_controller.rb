ad277302310b9e0eb52d7e13e14aa11559859b45
Brian Welch <myself@brianwelch.se>
2018-11-22 10:58:26 +0100

    improvement and dynamic rendering in profile show page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,9 +1,8 @@
 class ProfilesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show, :edit, :update]
-  before_action :set_user
+  before_action :set_user, :set_preferences, :set_allergies, :set_diets, :set_user_preferences, :set_user_allergies, :set_user_diets
 
   def show
-
   end
 
   def edit
@@ -17,4 +16,29 @@ class ProfilesController < ApplicationController
   def set_user
     @user = current_user
   end
+
+  def set_preferences
+    @all_preferences = FoodPreference.all
+  end
+
+  def set_allergies
+    @all_allergies = Allergy.all
+  end
+
+  def set_diets
+    @all_diets = SpecialDiet.all
+  end
+
+  def set_user_preferences
+    @user_preferences = current_user
+  end
+
+  def set_user_allergies
+    @user_allergies = current_user
+  end
+
+  def set_user_diets
+    @user_diets = current_user
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,13 +6,13 @@ module ApplicationHelper
 
   def toggle_form(args = {})
     return "<!--<form class='profile-toggle-form ' action='\#' method=' name='>--> \
-              <span>#{args[:type].capitalize}: <\/span><span class='profile-toggle-form-off'>off<\/span>
+              <td class='profile-personalization-parameter-column'>#{args[:type].capitalize}: <\/td><td><span class='profile-toggle-form-off'>off<\/span>
               <div class='toggle-button-frame'> \
                 <input type='hidden' value='#{args[:value]}' name='#{args[:type]}'> \
                 <input type='hidden' value='#{args[:user_id].id}' name='user_id'> \
                 <button class='toggle-button-button ' onclick=\"this.classList.toggle('turn-on-toggle');\" type='submit'> \
                 </button> \
-              </div><span class='profile-toggle-form-on'>on<\/span> \
+              </div><span class='profile-toggle-form-on'>on<\/span><\/td> \
             </form>"
   end
 
