5b2fb70e972a9acc9aa5aad9a55f0d17da357ea6
Brian Welch <myself@brianwelch.se>
2018-11-23 07:57:21 +0100

    seed file updated with remote JSON and different progress messaging

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,22 @@ require 'json'
 require 'open-uri'
 require 'nokogiri'
 require_relative 'users/user_seed.rb'
-require_relative 'methods/seed_methods'
+
+
+emoji_array = %w[🍏 🍎 🍐 🍊 🍋 🍌 🍉 🍇 🍓 🍈 🍒 🍑 🍍 🥭 🥥 🥝 🍅 🍆 🥑 🥦 🥒 🥬 🌶 🌽 🥕 🥔 🍠 🥐 🍞 🥖 🥨 🥯 🧀 🥚 🍳 🥞 🥓 🥩 🍗 🍖 🌭 🍔 🍟 🍕 🥪 🥙 🌮 🌯 🥗 🥘 🥫 🍝 🍜 🍲 🍛 🍣 🍱 🥟 🍤 🍙 🍚 🍘 🍥 🥮 🥠 🍢 🍡 🍧 🍨 🍦 🥧 🍰 🎂 🍮 🍭 🍬 🍫 🍿 🧂 🍩 🍪 🌰 🥜 🍯 🥛 🍼 ☕️ 🍵 🥤 🍶 🍺 🍻 🥂 🍷 🥃 🍸 🍹 🍾 🥄 🍴 🍽 🥣 🥡]
+
+wave = Enumerator.new do |e|
+  loop do
+    e.yield "    #{emoji_array[(rand(0..emoji_array.size))]} Working: "
+  end
+end
+progress = Thread.new do
+  loop do
+    printf("\r%s", wave.next)
+    sleep(0.3)
+  end
+end
+
 
 ## clears terminal window
 system 'clear'
@@ -14,11 +29,10 @@ puts '*' * 23
 puts '- ' * 30 + "\n"
 
 # starts goofy 'working' animation
-start_working
-sleep 2
+# start_working
+# sleep 2
 
 puts "\nDestroying the database!!"
-sleep 2
 
 Recipe.destroy_all
 RecipeIngredient.destroy_all
@@ -46,97 +60,101 @@ puts "\nDatabase destroyed."
 puts '- ' * 30 + "\n"
 
 
+# http://brianwelch.se/media/json_files/recipe_remote_02.json
 
-puts "\nPreparing to populate the database....."
-sleep 2
+puts "\nOpening remote JSON file....."
 
-# Iterates through every file in json_files directory
-Dir.glob("db/json_files/*.json").each do |json_file|
+recipe_file = JSON.parse(open("http://brianwelch.se/media/json_files/recipe_remote_02.json").read)
+recipe_file_array = recipe_file['body']["recipes"]
 
-  recipe_file = JSON.parse(File.read("#{json_file}"))
-  recipe_file_array = recipe_file['body']["recipes"]
+progress.kill
 
-  puts "\n\nProcessing recipe #{json_file}\n"
-  recipe_file_array.each do |recipe_array|
-    # Will NOT add recipes whose recipe steps aren't incremented
-    unless recipe_array["analyzedInstructions"].size.zero?
+index = 1
+recipe_file_array.each do |recipe_array|
 
-      # Creating Ingredient, MetricMeasure & ImperialMeasure Instances
-      recipe_array["extendedIngredients"].each_with_index do |ingredient_array, i|
+  printf("\r    Recipes: %d\/800", index)
 
-        # Will NOT create duplicate Ingredient Instances
-        unless !Ingredient.find_by_name(ingredient_array["name"]).nil?
-          Ingredient.create!(name: ingredient_array["name"])
-        end
 
-        # Will NOT create duplicate MetricMeasure Instances
-        unless !MetricMeasure.find_by_name(ingredient_array["measures"]["metric"]["unitShort"]).nil?
-          MetricMeasure.create!(name: ingredient_array["measures"]["metric"]["unitShort"])
-        end
+  # Will NOT add recipes whose recipe steps aren't incremented
+  unless recipe_array["analyzedInstructions"].size.zero? && (recipe_array["preparationMinutes"].nil? || recipe_array["preparationMinutes"].size.zero?)
 
-        # Will NOT create duplicate ImperialMeasure Instances
-        unless !ImperialMeasure.find_by_name(ingredient_array["measures"]["us"]["unitShort"]).nil?
-          ImperialMeasure.create!(name: ingredient_array["measures"]["us"]["unitShort"])
-        end
+    # Creating Ingredient, MetricMeasure & ImperialMeasure Instances
+    recipe_array["extendedIngredients"].each_with_index do |ingredient_array, i|
+
+      # Will NOT create duplicate Ingredient Instances
+      unless !Ingredient.find_by_name(ingredient_array["name"]).nil?
+        Ingredient.create!(name: ingredient_array["name"])
       end
 
-      # Creating Category Instances
-      recipe_array["cuisines"].each_with_index do |cuisines, i|
-        unless !Category.find_by_name(cuisines).nil?  || cuisines.size.zero?
-          Category.create!(name: cuisines)
-        end
+      # Will NOT create duplicate MetricMeasure Instances
+      unless !MetricMeasure.find_by_name(ingredient_array["measures"]["metric"]["unitShort"]).nil?
+        MetricMeasure.create!(name: ingredient_array["measures"]["metric"]["unitShort"])
       end
 
-      # Creating Recipe Instances - a big one
-      # Start by creating instructions array
-      analyzedInstructions_array = []
-      unless recipe_array["analyzedInstructions"].size.zero?
-        recipe_array["analyzedInstructions"][0]["steps"].each do |step|
-            analyzedInstructions_array << step["step"]
-        end
+      # Will NOT create duplicate ImperialMeasure Instances
+      unless !ImperialMeasure.find_by_name(ingredient_array["measures"]["us"]["unitShort"]).nil?
+        ImperialMeasure.create!(name: ingredient_array["measures"]["us"]["unitShort"])
       end
-      # NOW instanting Recipe model
-      unless !Recipe.find_by_name(recipe_array["title"]).nil?
-        Recipe.create!(name: recipe_array["title"],
-                       instructions: analyzedInstructions_array,
-                       photo_link: recipe_array["image"],
-                       prep_time: recipe_array["preparationMinutes"],
-                       cook_time: recipe_array["cookingMinutes"],
-                       servings: recipe_array["servings"],
-                       source: recipe_array["creditsText"])
-      end
-
-      # Creating RecipeCategory join table - Maybe move this outside the recipe array iteration
-      # Skip recipes which don't have any categories
-      unless !recipe_array["cuisines"].size.zero?
-        recipe_array["cuisines"].each do |category_name|
-          CategoryRecipe.create!(recipe: Recipe.find_by_name(recipe_array["title"]),
-                                 category: Category.find_by_name(category_name))
-        end
-      end
-
-      # Creating RecipeIngredient join table - Maybe move this outside the recipe array iteration
-      recipe_array["extendedIngredients"].each do |ingredient_array|
-
-
-        RecipeIngredient.create!(recipe: Recipe.find_by_name(recipe_array["title"]),
-                                 metric_quantity: ingredient_array["measures"]["metric"]["amount"],
-                                 imperial_quantity: ingredient_array["measures"]["us"]["amount"],
-                                 ingredient: Ingredient.find_by_name(ingredient_array["name"]),
-                                 imperial_measure: ImperialMeasure.find_by_name(ingredient_array["measures"]["us"]["unitShort"]),
-                                 metric_measure: MetricMeasure.find_by_name(ingredient_array["measures"]["metric"]["unitShort"]))
-      end
-
-    # This 'end' ends the unless condition which ignores recipes without incrememented instructions steps
     end
 
-  # This 'end' ends the iteration of 100 recipes or one json file
+    # Creating Category Instances
+    recipe_array["cuisines"].each_with_index do |cuisines, i|
+      unless !Category.find_by_name(cuisines).nil?  || cuisines.size.zero?
+        Category.create!(name: cuisines)
+      end
+    end
+
+    # Creating Recipe Instances - a big one
+    # Start by creating instructions array
+    analyzedInstructions_array = []
+    unless recipe_array["analyzedInstructions"].size.zero?
+      recipe_array["analyzedInstructions"][0]["steps"].each do |step|
+          analyzedInstructions_array << step["step"]
+      end
+    end
+    # NOW instanting Recipe model
+    unless !Recipe.find_by_name(recipe_array["title"]).nil?
+      Recipe.create!(name: recipe_array["title"],
+                     instructions: analyzedInstructions_array,
+                     photo_link: recipe_array["image"],
+                     prep_time: recipe_array["preparationMinutes"],
+                     cook_time: recipe_array["cookingMinutes"],
+                     servings: recipe_array["servings"],
+                     source: recipe_array["creditsText"])
+    end
+
+    # Creating RecipeCategory join table - Maybe move this outside the recipe array iteration
+    # Skip recipes which don't have any categories
+    unless !recipe_array["cuisines"].size.zero?
+      recipe_array["cuisines"].each do |category_name|
+        CategoryRecipe.create!(recipe: Recipe.find_by_name(recipe_array["title"]),
+                               category: Category.find_by_name(category_name))
+      end
+    end
+
+    # Creating RecipeIngredient join table - Maybe move this outside the recipe array iteration
+    recipe_array["extendedIngredients"].each do |ingredient_array|
+
+
+      RecipeIngredient.create!(recipe: Recipe.find_by_name(recipe_array["title"]),
+                               metric_quantity: ingredient_array["measures"]["metric"]["amount"],
+                               imperial_quantity: ingredient_array["measures"]["us"]["amount"],
+                               ingredient: Ingredient.find_by_name(ingredient_array["name"]),
+                               imperial_measure: ImperialMeasure.find_by_name(ingredient_array["measures"]["us"]["unitShort"]),
+                               metric_measure: MetricMeasure.find_by_name(ingredient_array["measures"]["metric"]["unitShort"]))
+    end
+
+  # This 'end' ends the unless condition which ignores recipes without incrememented instructions steps
   end
-# this 'end' ends the iteration through all json files in the directory
+
+  index += 1
+
+# This 'end' ends the iteration of 100 recipes or one json file
 end
 
+
 puts "\nRecipes created."
-sleep 1
+sleep 2
 
 puts "\nCreating users"
 sleep 2
@@ -187,8 +205,9 @@ sleep 2
 special_diets.each do |diet|
   SpecialDiet.create!(name: diet)
 end
-puts "\nAllergies Created!"
+puts "\nAllergies Created!\n"
+puts '- ' * 30 + "\n"
+puts '*' * 23
+puts ' Seeding Completed'
+puts '*' * 23
 
-
-# kills goofy 'working' animation
-start_working.exit
