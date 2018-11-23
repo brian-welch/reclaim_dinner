cba22f5fa6a3fb15a23b83cc4b151fea7dd9491d
Beltran Sainz de Vicuna Nowack <gorthor@gmail.com>
2018-11-23 15:16:01 +0100

    new infor card, now with animation

--- app/javascript/components/recipe-information-toggle.js
+++ app/javascript/components/recipe-information-toggle.js
@@ -1,8 +1,8 @@
 function recipeInformationToggle() {
 const recipeImg = document.querySelectorAll(".dish-image");
 
-  $(".dish-image, .info-card").hover(function () {
-    $(".info-card").toggleClass("open");
+  $(".dish-image, .recipe-detail-block-outer").hover(function () {
+    $(".recipe-detail-block-outer").toggleClass("open");
   });
 };
 export { recipeInformationToggle };
