1723fa2ff482b6e26d03aaac66df6046d14f4dcc
Brian Welch <myself@brianwelch.se>
2018-11-23 18:18:14 +0100

    remove redundant files from db folder

--- app/javascript/components/recipe-information-toggle.js
+++ app/javascript/components/recipe-information-toggle.js
@@ -2,11 +2,11 @@ function recipeInformationToggle() {
 const recipeImg = document.querySelectorAll(".dish-image");
 
 
-  $(".dish-image, .info-card").hover(function () {
+  $(".dish-image, .recipe-detail-block-outer, .recipe-detail-block-inner, .recipe-detail-photo-box, .recipe-detail-info-box, .recipe-detail-info-name, .recipe-detail-info-specs ").hover(function () {
 
-    var num = $(this).attr("id").replace("dish-","");
+    // var num = $(this).attr("id").replace("dish-","");
 
-    var card_id = "info_card-" + num;
+    var card_id = "info_card-" + $(this).attr("id").replace("dish-","");;
 
     $("#" + card_id).toggleClass("open");
   });
