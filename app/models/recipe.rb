faa5473385c3fc1fa4e12f6abba5efb8efdda9fb
Makena Hawley <makena.hawley@gmail.com>
2018-11-26 15:49:46 +0100

    added logic to show the correct image depending on the recipe for the show page, and re-aligned icons on the left

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,29 @@ class Recipe < ApplicationRecord
   has_many :recipe_ingredients, dependent: :destroy
   has_many :user_recipes
   has_many :recipe_ratings
+
+  def list_of_icons
+    options =[
+     :vegetarian,
+     :vegan,
+     :glutenFree,
+     :dairyFree,
+     # :pork,
+     # :fish,
+     # :shellfish,
+     # :peanut,
+     # :tofu,
+     # :egg
+    ]
+    options.select do |option|
+      self.public_send(option)
+    end.map do |result|
+      result.to_s + '.svg'
+    end
+  end
 end
+
+# Goes sandwicehd between selevt and map starts line 21. this end followed by map.
+# .reject do |option|
+#       self.vegan && [:vegetarian, :dairyFree].include? option
+#     end
