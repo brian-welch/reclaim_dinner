ea11395f869c9d9ab36067aded6c0ee16a590ae4
Makena Hawley <makena.hawley@gmail.com>
2018-11-28 18:06:27 +0100

    beginning logic of backend shopping list. need to work on route and view tomorrow

--- app/controllers/user_recipes_controller.rb
+++ app/controllers/user_recipes_controller.rb
@@ -1,35 +1,22 @@
 class UserRecipesController < ApplicationController
 
-  # def index
-  #   if current_user.nil?
-  #     @five_recipes = []
-  #     5.times.with_index { @five_recipes << Recipe.all.sample }
-  #     @five_recipes
-  #   else
-  #     @five_recipes = []
-  #     5.times.with_index { @five_recipes << Recipe.all.sample }
-  #     @five_recipes
-  #   end
-  # end
-
   def create
+    day = (Date.today + 7).beginning_of_week
+    user_recipes = []
+    # WIPES DB OF ALL USER RECIPES
+    # DELETE AFTER TESTING IS DONE
+    UserRecipe.destroy_all
+    %w(monday_id tuesday_id wednesday_id thursday_id friday_id).each do |id|
+      user_recipe = UserRecipe.new
+      recipe      = Recipe.find(params[id])
+      user_recipe.recipe = recipe
+      user_recipe.user   = current_user
+      user_recipe.chosen_date = day
+      day += 1
+      user_recipe.save
+      user_recipes << user_recipe
+    end
+
+    redirect_to profile_path
   end
-  #   @recipe_list = []
-  #   5.times.with_index { @recipe_ingredients << RecipeIngredient.where(recipe_id: params[:id]) }
-  # #   @recipe_list.each....
-    # redirect_to shopping_list_path
 
- # def set_user
- #    @user = current_user
- # end
-
- def set_date
-  @chosen_date = Date.today.beginning_of_week
-  @Monday = @chosen_date
-  @Tuesday = @chosen_date + 1
-  @Wednesday = @chosen_date + 2
-  @Thursday = @chosen_date + 3
-  @Friday = @chosen_date + 4
- end
-
-end
