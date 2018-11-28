18f94d5d219a409872c5783b24f907ca0395b2cd
Makena Hawley <makena.hawley@gmail.com>
2018-11-28 16:44:28 +0100

    began work on the grocery list..it's not finished.

--- app/controllers/user_recipes_controller.rb
+++ app/controllers/user_recipes_controller.rb
@@ -1,9 +1,35 @@
 class UserRecipesController < ApplicationController
 
-  def create
-    # raise
-  end
+  # def index
+  #   if current_user.nil?
+  #     @five_recipes = []
+  #     5.times.with_index { @five_recipes << Recipe.all.sample }
+  #     @five_recipes
+  #   else
+  #     @five_recipes = []
+  #     5.times.with_index { @five_recipes << Recipe.all.sample }
+  #     @five_recipes
+  #   end
+  # end
 
-  private
+  def create
+  end
+  #   @recipe_list = []
+  #   5.times.with_index { @recipe_ingredients << RecipeIngredient.where(recipe_id: params[:id]) }
+  # #   @recipe_list.each....
+    # redirect_to shopping_list_path
+
+ # def set_user
+ #    @user = current_user
+ # end
+
+ def set_date
+  @chosen_date = Date.today.beginning_of_week
+  @Monday = @chosen_date
+  @Tuesday = @chosen_date + 1
+  @Wednesday = @chosen_date + 2
+  @Thursday = @chosen_date + 3
+  @Friday = @chosen_date + 4
+ end
 
 end
---
+++ app/javascript/packs/recipe-form.js
@@ -0,0 +1,1 @@
+console.log("recipe form is loading!");
